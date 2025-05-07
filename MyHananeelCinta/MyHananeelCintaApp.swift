ee50fd6676d87ae14a35267eff6bf6172878d51e
if414028 <voldarex.napitupulu@gmail.com>
2025-05-07 13:11:25 +0700

    Initial Commit

---
+++ MyHananeelCinta/MyHananeelCintaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHananeelCintaApp.swift
+//  MyHananeelCinta
+//
+//  Created by Advotics on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHananeelCintaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHananeelCintaTests/MyHananeelCintaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyHananeelCintaTests.swift
+//  MyHananeelCintaTests
+//
+//  Created by Advotics on 07/05/25.
+//
+
+import Testing
+@testable import MyHananeelCinta
+
+struct MyHananeelCintaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
