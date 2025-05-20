69820f49e599bbb2a7bb4452856528fa06e9efc3
if414028 <voldarex.napitupulu@gmail.com>
2025-05-20 15:41:19 +0700

    Connect project to firebase services

--- MyHananeelCinta/MyHananeelCintaApp.swift
+++ MyHananeelCinta/MyHananeelCintaApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("Firebase configured!")
+
+    return true
+  }
+}
 
 @main
 struct MyHananeelCintaApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
--- MyHananeelCinta/Views/Login/LoginView.swift
+++ MyHananeelCinta/Views/Login/LoginView.swift
@@ -5,3 +5,47 @@
 //  Created by Advotics on 20/05/25.
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    @State private var isActive = false
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+    
+    var body: some View {
+        if isActive {
+            //LoginView()
+        } else {
+            VStack {
+                Spacer()
+                
+                // Logo di tengah
+                Image("AppLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                
+                Spacer()
+                
+                // Versi aplikasi di bawah
+                Text("Version \(appVersion)")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white) // Ganti warna jika perlu
+            .onAppear {
+                // Delay 3 detik sebelum pindah
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
